99a0ae027dd3a5ef34f08aae17094f777bef3e49
sandond77 <sandu@ucdavis.edu>
2018-05-30 22:36:38 -0700

    fixing portfolio route

--- routes/routes.js
+++ routes/routes.js
@@ -16,28 +16,24 @@ router.get("/portfolio", function(req,res){
 	res.sendFile(path.join(__dirname,"../public/views/portfolio.html"))
 });
 
-// router.get("*", function(req,res){
-// 	res.sendFile(path.join(__dirname,"../public/views/index.html"))
-// });
+router.get("*", function(req, res, next){
+  res.status(404);
 
-// router.get("*", function(req, res, next){
-//   res.status(404);
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
 
-//   // respond with html page
-//   if (req.accepts('html')) {
-//     res.render('404', { url: req.url });
-//     return;
-//   }
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
 
-//   // respond with json
-//   if (req.accepts('json')) {
-//     res.send({ error: 'Not found' });
-//     return;
-//   }
-
-//   // default to plain-text. send()
-//   res.type('txt').send('Not found');
-// });
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 module.exports = router;
 
