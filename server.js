3cafd3c8432915e3aaf9aaa10a9bd5cb42004606
sandond77 <sandu@ucdavis.edu>
2018-06-12 21:45:10 -0700

    added sevafund to portfolio page

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 var routes = require("./routes/routes.js");
 var PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'html');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', 'html');
 
 app.use(routes);
 
