ec1c2195386b430ab108b7a87a074fd735fe4e98
sandond77 <sandu@ucdavis.edu>
2018-07-10 22:43:25 -0700

    removed old and test alerts

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -10,10 +10,6 @@ $(document).ready(function(){
 	  		message: $('#message').val().trim()
 	  	}
 
-	  	// $('#messageModal').modal('show');
-	  	// var successful = '<div class="alert alert-success alert-dismissible collapse" id="successful" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>Your message was successfully sent!</p></div>'
-	  	// $('#append').append(successful);
-	  	// $('#successful').show();
 	  	$.ajax({
 	  		method:"POST",
 	  		url: "/send",
@@ -23,14 +19,11 @@ $(document).ready(function(){
 	  			$('#name').val("")
 		  		$('#email').val("")
 		  		$('#subject').val("")
-		  		$('#message').val("")
-				  // alert("Your message was sent")
-				// $('#messageModal').modal('show');
+		  		$('#message').val("");
 			  	var successful = '<div class="alert alert-success alert-dismissible collapse" id="successful" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>Your message was successfully sent!</p></div>'
 			  	$('#append').append(successful);
 			  	$('#successful').show();
 	  		} else {
-	  			// alert("There was an error sending your message. Please try again later");
 			  	var failure = '<div class="alert alert-danger alert-dismissible collapse" id="failure" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>Your message was not sent. Please try again later.</p></div>'
 			  	$('#append').append(failure);
 			  	$('#failure').show();
